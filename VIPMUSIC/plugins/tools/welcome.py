d2e0d4c2f2a75746db84a9e562891709c7a106ca
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2024-02-09 15:13:58 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -103,15 +103,16 @@ async def handle_member_update(client: app, member: ChatMemberUpdated):
 
             # Welcome message for new members
             caption = (
-            f"**𝐇ᴇʏ  𝐁ᴀʙʏ...»
-{member.new_chat_member.user.mention} \n\n✰𝐖ᴇʟᴄᴏᴍᴇ  𝐓ᴏ  𝐆ʀᴏᴜᴘ✰**\n\n"
-            f"**{chat.title}**\n\n"
-            f"**▬▭▬▭▬▭▬▭▬**\n"
-            f"**✰𝐌ᴇᴍʙᴇʀ 𝐈ɴꜰᴏʀᴍᴀᴛɪᴏɴ✰ \n▬▭▬▭▬▭▬▭▬**\n"
-            f"**✰𝐍𝐚𝐦𝐞➠ {member.new_chat_member.user.mention}**\n"
-            f"**✰𝐈𝐃➠ {member.new_chat_member.user.id}**\n"
-            f"**✰𝐔𝐬𝐞𝐫➠ @{member.new_chat_member.user.username}**\n▬▭▬▭▬▭▬▭▬**\n"
-            f"**✰𝐓ᴏᴛᴀʟ 𝐌ᴇᴍʙᴇʀ»» {count}**"
+                        f"**𝐇ᴇʏ»» {member.new_chat_member.user.mention} ✰✰**\n\n**🅦𝐞᭄𝗹𝗰𝗼𝗺𝗲 ✪  𝐓ᴏ👇👇 ✰✰**\n\n"
+            f"**┏━━•❅•°• ❈  •°•❅•━━┓**\n"
+            f"**  {chat.title}  ** \n"
+            f"**┗━━•❅•°• ❈  •°•❅•━━┛**\n\n"
+            f"**☞𝐌ᴇᴍʙᴇʀ 𝐈ɴꜰᴏʀᴍᴀᴛɪᴏɴ☜\n╭✠╼━━━❖━━━━━✠╮\n"
+            f"**│•𝐍𝐞𝐰 𝐁𝐮𝐝𝐝𝐲 ➠ {member.new_chat_member.user.mention}**\n"
+            f"**│•𝐈𝐃➠** {member.new_chat_member.user.id} \n"
+            f"**│•𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞➠ @{member.new_chat_member.user.username}**\n╰✠╼━━━━━━❖━━━━━━✠╯\n"
+            f"**☞𝐋ɪɴᴋ» @{chat.username}**\n"
+            f"**☞𝐓ᴏᴛᴀʟ 𝐌ᴇᴍʙᴇʀ 𝐍ᴏᴡ»» {count}**"
             )
             add_button_text = "🌿 𝐊ɪᴅɴᴀᴘ 𝐌ᴇ 🌿"
 
