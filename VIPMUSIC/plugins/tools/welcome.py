b19c90bebd7222e63f513b98a32e0b144dfc332b
MrH4CK3R474 <148775562+MrH4CK3R474@users.noreply.github.com>
2024-02-09 15:15:39 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -103,7 +103,8 @@ async def handle_member_update(client: app, member: ChatMemberUpdated):
 
             # Welcome message for new members
             caption = (
-                        f"**𝐇ᴇʏ»» {member.new_chat_member.user.mention} ✰✰**\n\n**🅦𝐞᭄𝗹𝗰𝗼𝗺𝗲 ✪  𝐓ᴏ👇👇 ✰✰**\n\n"
+                        f"**𝐇ᴇʏ  𝐁ᴀʙʏ...»» 
+{member.new_chat_member.user.mention} ✰✰**\n\n**✰𝐖ᴇʟᴄᴏᴍᴇ  𝐓ᴏ  𝐆ʀᴏᴜᴘ✰**\n\n"
             f"**┏━━•❅•°• ❈  •°•❅•━━┓**\n"
             f"**  {chat.title}  ** \n"
             f"**┗━━•❅•°• ❈  •°•❅•━━┛**\n\n"
